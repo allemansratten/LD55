b866687a86aeae63406f8f085b849301b59d4e28
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-13 12:34:13 +0200

    first shot pathfinding

---
+++ Assets/TempVilda/Soldier.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Soldier : MonoBehaviour
+{
+    public string team;
+    [SerializeField] private Transform movePositionTransform;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine("FindNewEnemy");
+    }
+
+    IEnumerator FindNewEnemy() {
+        while(true) {
+            var my_pos = GetComponent<Transform>().position;
+            var soldiers = FindObjectsOfType<Soldier>();
+
+            float? min_dist = null;
+            Soldier? min_soldier = null;
+
+
+            foreach(var soldier in soldiers) {
+                // skip our team
+                if (soldier.team == team) {
+                    continue;
+                }
+
+                var soldier_pos = soldier.GetComponent<Transform>().position;
+                var soldier_dist = Vector3.Distance(my_pos, soldier_pos);
+                if (min_dist is null || soldier_dist < min_dist) {
+                    min_dist = soldier_dist;
+                    min_soldier = soldier;
+                }
+            }        
+            // repeatedly find nearest soldier
+            navMeshAgent.destination = min_soldier.GetComponent<Transform>().position;
+
+            // repeat every second
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider) {
+
+        var enemy = collider.gameObject.GetComponent<Soldier>();
+        // we did not collide with enemy
+        if (enemy is null) {
+            return;
+        }
+
+        if(Random.value > 0.5f) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() { 
+    }
+}
