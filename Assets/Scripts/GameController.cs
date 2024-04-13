930775b69351150881d6debefb8fe9c2d85930d5
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-04-13 13:29:45 +0200

    Add team toggle

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject guyPrefab;
+    public GameObject teamToggle;
+
+    public bool spawnRedTeam = true;
+
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(guyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
 
+        SetSpawnRedTeam(true);
     }
 
     // Update is called once per frame
@@ -22,5 +28,11 @@ public class GameController : MonoBehaviour
     public void StartBattle()
     {
         Debug.Log("Battle started");
+    }
+
+    public void SetSpawnRedTeam(bool isRedTeam)
+    {
+        spawnRedTeam = isRedTeam;
+        teamToggle.GetComponent<Image>().color = isRedTeam ? Color.red : Color.cyan;
     }
 }
