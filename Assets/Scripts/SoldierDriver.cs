aec27306713d31a2f7843b712fb7eece959f9ef7
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-04-14 14:18:22 +0200

    Deprecate UnitBasic

--- Assets/Scripts/SoldierDriver.cs
+++ Assets/Scripts/SoldierDriver.cs
@@ -7,7 +7,8 @@ public class SoldierDriver : MonoBehaviour
     {
         EventManager.OnBattleStart += () =>
         {
-            foreach(var soldier in FindObjectsOfType<Soldier>()) {
+            foreach (var soldier in FindObjectsOfType<Soldier>())
+            {
                 soldier.enabled = true;
             }
         };
@@ -15,15 +16,19 @@ public class SoldierDriver : MonoBehaviour
 
     void SpawnDebugEnemies()
     {
-        Spawn("B", "UnitBasic", new Vector3(3, 0, 3));
+        Spawn("B", "UnitMelee", new Vector3(3, 0, 3));
         //Spawn("B", "UnitArcher", new Vector3(4, 0, 3));
         //Spawn("B", "UnitArcher", new Vector3(5, 0, 3));
-        //Spawn("B", "UnitBasic", new Vector3(6, 0, 3));
-        //Spawn("B", "UnitBasic", new Vector3(7, 0, 3));
+        //Spawn("B", "UnitMelee", new Vector3(6, 0, 3));
+        //Spawn("B", "UnitMelee", new Vector3(7, 0, 3));
     }
 
     public void Spawn(string team, string unitType, Vector3 position)
     {
+        if (unitType == "UnitBasic")
+        {
+            throw new System.Exception("Spawning UnitBasic is not allowed. Use UnitMelee.");
+        }
         GameObject unitPrefab = Resources.Load<GameObject>("Units/" + unitType);
         var soldier = Instantiate(unitPrefab, position, Quaternion.identity).GetComponent<Soldier>();
         soldier.SetTeam(team);
@@ -31,6 +36,10 @@ public class SoldierDriver : MonoBehaviour
 
     public void SpawnSquad(string team, string unitType, Vector3 position)
     {
+        if (unitType == "UnitBasic")
+        {
+            throw new System.Exception("Spawning UnitBasic is not allowed. Use UnitMelee.");
+        }
         GameObject newGameObject = new GameObject("Squad/" + unitType);
         UnitSquad unitSquad = newGameObject.AddComponent<UnitSquad>();
         GameObject unit = Resources.Load<GameObject>("Units/" + unitType);
